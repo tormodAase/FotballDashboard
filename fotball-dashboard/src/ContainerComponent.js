8f697ed3433b7f2bad8df9324582f2020602f353
Tormod Aase <tormod.aase@bouvet.no>
2020-12-27 18:57:26 +0100

    Basic container added

--- fotball-dashboard/src/ContainerComponent.js
+++ fotball-dashboard/src/ContainerComponent.js
@@ -1,8 +1,14 @@
 import react from 'react';
+import './App.css';
 
 let Container = (props) => {
     let title = props.title;
-    return <div>{props.children}</div>
+    return (<div className="Container">
+            <div className="ContainerHeader"><h3>{props.title}</h3></div>
+            <div className="ContainerBody">
+                {props.children}
+            </div>
+        </div>);
 }
 
 export default Container;
