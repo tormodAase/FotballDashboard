a30ba1b54191032be6af5d8d11fb63f429f8ccdc
Tormod Aase <tormod.aase@bouvet.no>
2020-12-27 20:12:32 +0100

    Added errormessage to tablecomponent

--- fotball-dashboard/src/TableComponent.js
+++ fotball-dashboard/src/TableComponent.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './App.css';
 
 let Table = (props) => {
-    return <div>TableComponent</div>;
+    let table = "";
+    if (props.array === undefined) {
+        table = <div className="ErrorMessage">ERROR: props.array is undefined. Please contact Tormod for assistance</div>
+    } else if (props.array[0] === undefined) {
+        table = <div className="ErrorMessage">ERROR: props.array[0] is undefined. Please contact Tormod for assistance</div>
+    }
+    return table;
 }
 
 export default Table;
