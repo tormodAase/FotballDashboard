7c41ad3b1e216e3a9a3604daab9a77871460f7db
Tormod Aase <tormod.aase@bouvet.no>
2020-12-30 19:00:56 +0100

    DatabaseService started

--- backend/src/com/tormodaase/Main.java
+++ backend/src/com/tormodaase/Main.java
@@ -1,8 +1,27 @@
 package com.tormodaase;
 
+import com.tormodaase.footballAPI.SQL.DatabaseService;
+import com.tormodaase.footballAPI.objects.Country;
+import com.tormodaase.footballAPI.objects.exceptions.JSONMismatchException;
+import org.json.JSONObject;
+
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Hello world");
+    public static void main(String[] args) throws JSONMismatchException, SQLException {
+	    String connectionString = "";
+	    String username = "a_FotballDashboard";
+	    String password = "";
+	    JSONObject json = new JSONObject();
+	    json.put("name", "Norway");
+	    json.put("code", "no");
+	    json.put("flag", "TestValue");
+	    Country country = new Country(json);
+
+	    DatabaseService.connect(connectionString, username, password);
+	    DatabaseService.upsertCountry(country);
+	    DatabaseService.disconnect();
+
     }
 }
--- backend/src/com/tormodaase/footballAPI/SQL/DatabaseService.java
+++ backend/src/com/tormodaase/footballAPI/SQL/DatabaseService.java
@@ -1,4 +1,35 @@
 package com.tormodaase.footballAPI.SQL;
 
+import com.tormodaase.footballAPI.objects.Country;
+
+import java.sql.*;
+
 public class DatabaseService {
+    private static Connection connection;
+    public static void connect(String connectionString, String username, String password) throws SQLException {
+        Connection connection = DriverManager.getConnection(connectionString, username, password);
+    }
+
+    public static void disconnect() throws SQLException {
+        connection.close();
+    }
+
+    public static void upsertItem(Object object) throws SQLException {
+        if (object.getClass().equals(Country.class)) {
+            upsertCountry((Country) object);
+        }
+        //TODO create more cases
+    }
+
+    public static void upsertCountry(Country country) throws SQLException {
+        //TODO
+        String[] values = {country.getName(), country.getCode(), country.getFlag()};
+        String query = "INSERT into dbo.countries(name, code, flag)"
+                +       "VALUES("
+                +       String.join(",", values)
+                +")";
+
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+    }
 }
